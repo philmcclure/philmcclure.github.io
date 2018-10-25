b10342287906e4af2a985218daafa3cb303cb8ce
philmcclure <philip.mcclure@gmail.com>
2018-10-25 05:56:53 -0500

    added types 'r' and 's' thermocouples to thermocouple_identification

--- thermocouple_identification/js/script.js
+++ thermocouple_identification/js/script.js
@@ -1,9 +1,11 @@
-var thermocoupleList = ["J-Type", "K-Type", "T-Type", "E-Type", "N-Type"];
+var thermocoupleList = ["J-Type", "K-Type", "T-Type", "E-Type", "N-Type", "R-Type", "S-Type"];
 var j = ["Iron Fe (magnetic)", "Constantan Copper-Nickel Cu-Ni", "j.png", "32 to 1382°F", "0 to 750°C", "–346 to 2193°F", "–210 to 1200°C", "-8.095 to 69.553", "greater of 2.2°C or 0.75%", "greater of 1.1°C or 0.4%", "j-iec.png", "Reducing, Vacuum, Inert. Limited Use in Oxidizing at High Temperatures. Not Recommended for Low Temperatures."];
 var k = ["Chromega NickelChromium Ni-Cr", "Alomega Nickel-Aluminum Ni-Al (magnetic)", "k.png", "–328 to 2282°F", "–200 to 1250°C", "–454 to 2501°F", "–270 to 1372°C", "-6.458 to 54.886", "greater of 2.2°C or 0.75%", "greater of 1.1°C or 0.4%", "k-iec.png", "Clean Oxidizing and Inert. Limited Use in Vacuum or Reducing. Wide Temperature Range, Most Popular Calibration"];
 var t = ["Copper Cu", "Constantan Copper-Nickel Cu-Ni", "t.png", "–328 to 662°F", "–250 to 350°C", "–454 to 752°F", "–270 to 400°C", "–6.258 to 20.872", "greater of 1.0°C or 0.75%", "greater of 0.5°C or 0.4%", "t-iec.png", "Mild Oxidizing, Reducing Vacuum or Inert. Good Where Moisture Is Present. Low Temperature and Cryogenic Applications"];
 var e = ["Chromega NickelChromium Ni-Cr", "Constantan Copper-Nickel Cu-Ni", "e.png", "–328 to 1652°F", "–200 to 900°C", "–454 to 1832°F", "–270 to 1000°C", "–9.835 to 76.373", "greater of 1.7°C or 0.5%", "greater of 1.0°C or 0.4%", "e-iec.png", "Oxidizing or Inert. Limited Use in Vacuum or Reducing. Highest EMF Change Per Degree"];
 var n = ["Omega-P Nicrosil Ni-Cr-Si", "Omega-N Nisil Ni-Si-Mg", "n.png", "–450 to 2372°F", "–270 to 1300°C", "–450 to 2372°F", "–270 to 1300°C", "–4.345 to 47.513", "greater of 2.2°C or 0.75%", "greater of 1.1°C or 0.4%", "n-iec.png", "Alternative to Type K. More Stable at High Temps"];
+var r = ["Platinum-13% Rhodium Pt-13% Rh", "Platinum Pt", "r.png", "32 to 2642°F", "0 to 1450°C", "–58 to 3214°F", "–50 to 1768°C", "–0.226 to 21.101", "greater of 1.5°C or 0.25%", "greater of 0.6°C or 0.1%", "r-iec.png", "Oxidizing or Inert. Do Not Insert in Metal Tubes. Beware of Contamination. High Temperature"];
+var s = ["Platinum-10% Rhodium Pt-10% Rh", "Platinum Pt", "s.png", "32 to 2642°F", "0 to 1400°C", "–58 to 3214°F", "–50 to 1768°C", "–0.236 to 18.693", "greater of 1.5°C or 0.25%", "greater of 0.6°C or 0.1%", "s-iec.png", "Oxidizing or Inert. Do Not Insert in Metal Tubes. Beware of Contamination. High Temperature"];
 
 // populate dropdown lists function
 
@@ -67,6 +69,32 @@ function showTheDamnNumber() {
         document.getElementById("specialLimitError").innerHTML   = '<strong>Special Limits of Error: </strong>' + e[9];
         document.getElementById("iec584_3").src                  = 'img/' + e[10];
         document.getElementById("comments").innerHTML            = '<strong>Comments: </strong>' + e[11];
+    }
+
+    if (tcouple == "R-Type") {
+        document.getElementById("pluslead").innerHTML            = '<strong>+ Lead: </strong>' + r[0];
+        document.getElementById("neglead").innerHTML             = '<strong>- Lead: </strong>' + r[1];
+        document.getElementById("grade").src                     = 'img/' + r[2];
+        document.getElementById("maxUsefulTempFahr").innerHTML   = '<strong>Max Useful Temp Range: </strong>' + r[3] + ' / ' + n[4];
+        document.getElementById("maxGradeTempFahr").innerHTML    = '<strong>Max Grade Temp Range: </strong>' + r[5] + ' / ' + n[6];
+        document.getElementById("emfOverMaxTempRange").innerHTML = '<strong>EMF (mV) Over Max Temp Range: </strong>' + r[7];
+        document.getElementById("standardLimitError").innerHTML  = '<strong>Standard Limits of Error: </strong>' + r[8];
+        document.getElementById("specialLimitError").innerHTML   = '<strong>Special Limits of Error: </strong>' + r[9];
+        document.getElementById("iec584_3").src                  = 'img/' + r[10];
+        document.getElementById("comments").innerHTML            = '<strong>Comments: </strong>' + r[11];
+    }
+
+    if (tcouple == "S-Type") {
+        document.getElementById("pluslead").innerHTML            = '<strong>+ Lead: </strong>' + s[0];
+        document.getElementById("neglead").innerHTML             = '<strong>- Lead: </strong>' + s[1];
+        document.getElementById("grade").src                     = 'img/' + s[2];
+        document.getElementById("maxUsefulTempFahr").innerHTML   = '<strong>Max Useful Temp Range: </strong>' + s[3] + ' / ' + n[4];
+        document.getElementById("maxGradeTempFahr").innerHTML    = '<strong>Max Grade Temp Range: </strong>' + s[5] + ' / ' + n[6];
+        document.getElementById("emfOverMaxTempRange").innerHTML = '<strong>EMF (mV) Over Max Temp Range: </strong>' + s[7];
+        document.getElementById("standardLimitError").innerHTML  = '<strong>Standard Limits of Error: </strong>' + s[8];
+        document.getElementById("specialLimitError").innerHTML   = '<strong>Special Limits of Error: </strong>' + s[9];
+        document.getElementById("iec584_3").src                  = 'img/' + s[10];
+        document.getElementById("comments").innerHTML            = '<strong>Comments: </strong>' + s[11];
     }
 
     if (tcouple == "N-Type") {
